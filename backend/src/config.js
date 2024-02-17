479739be371a2e3abf33cb85ef521dd91fab1c21
Elviajerocsl <loscaboslemonade@gmail.com>
2024-02-17 06:01:31 -0700

    updatemint

--- backend/src/config.js
+++ backend/src/config.js
@@ -58,13 +58,13 @@ const MINT_PRICE = 100; // Minting price per NFT. Goerli = ETH, Ethereum = ETH, 
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2024-06-15T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2024-02-15T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2024-02-10T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x1e4Bd4A1f69A3cF7BCb3271C884d2aFEFB2c18f8"; // Address that will receive the royalty
-const BASE_URI = "2024-05-10T11:30:48+00:00"; // only update if you want to manually set the base uri
+const BASE_URI = "2024-02-10T11:30:48+00:00"; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
